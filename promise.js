61e2e74645ee7b13bbe57e4ca07cea34863feba5
Buu Nguyen <buunguyen@gmail.com>
2014-02-26 17:54:17 -0800

    Refactor

--- promise.js
+++ promise.js
@@ -1,47 +1,61 @@
-var nextTick = (process && process.nextTick) || setImmediate || setTimeout
-
-function isObject(o) { return o && typeof o === 'object'}
-function isFunction(f) { return f && typeof f === 'function'}
-
-function Promise() {
-  var state     = 'pending', 
-      callbacks = [],
-      value
-  function then(onFulfilled, onRejected) {
-    var p = new Promise()
-    function callback() {
-      var ret = value, 
-          fn  = state === 'resolved' ? 'resolve' : 'reject', 
-          cb  = state === 'resolved' ? onFulfilled : onRejected
-      try {
-        if (isFunction(cb)) {
-          ret = cb(value)
-          fn  = 'resolve'
-        }
-        if (ret === p) return p.reject(new TypeError('Can\'t resolve itself'))
-        if (ret instanceof Promise) return ret.then(p.resolve, p.reject)
-        p[fn](ret)
-      } catch (e) {
-        p.reject(e)
+;(function() {
+  function Promise() {
+    var state     = 'pending', 
+        callbacks = [],
+        value
+    function then(onFulfill, onReject) {
+      var p = new Promise()
+      function callback() {
+        var fn = state === 'resolved' ? 'resolve' : 'reject', 
+            cb = state === 'resolved' ? onFulfill : onReject
+        if (typeof cb === 'function') {
+          try {
+            var ret = cb(value)
+            if (ret === p) return p.reject(new TypeError('Cannot resolve itself'))
+            if (ret instanceof Promise) return ret.then(p.resolve, p.reject)
+            p.resolve(ret)
+          } catch (e) {
+            p.reject(e)
+          }
+        } else p[fn](value)
+      }
+      if (state === 'pending') callbacks.push(callback)
+      else nextTick(callback)
+      return p
+    }
+    function complete(_state) {
+      return function(_value) {
+        if (state !== 'pending') return
+        state = _state
+        value = _value
+        nextTick(function() {
+          for (var cb; cb = callbacks.shift();) cb()
+        })
       }
     }
-    if (state === 'pending') callbacks.push(callback)
-    else nextTick(callback)
+    this.then    = then
+    this.resolve = complete('resolved')
+    this.reject  = complete('rejected')
+  }
+
+  Promise.all = function(promises) {
+    var p = new Promise(), 
+        values = []
+    ;(function next(index) {
+      promises[index].then(function(value) {
+        values[index++] = value
+        if (index < promises.length) next(index)
+        else p.resolve(values)
+      }, p.reject)
+    })(0)
     return p
   }
-  function complete(_state) {
-    return function(_value) {
-      if (state !== 'pending') return
-      state = _state
-      value = _value
-      for (var cb; cb = callbacks.shift();) nextTick(cb)
-    }
-  }
-  this.then    = then
-  this.resolve = complete('resolved')
-  this.reject  = complete('rejected')
-}
 
-if (isObject(module) && isObject(module.exports)) 
-  module.exports = Promise
-else window.Promise = Promise
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Promise
+    var nextTick = process.nextTick
+  } else {
+    window.Promise = Promise
+    var nextTick = window.setImmediate || window.setTimeout
+  }
+})()
